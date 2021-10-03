a29f04799d3e95064906618886b87c2962a3fd16
Douglas Barone <douglasvbarone@gmail.com>
2021-10-03 18:29:31 -0400

    Mudança no contexto

--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ import express from 'express'
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
 
-import * as db from './fakeDB'
+import { pets, owners, categories } from './fakeDB'
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ ...db })
+    context: () => ({ pets, owners, categories })
   })
+
   await server.start()
+
   const app = express()
+
   server.applyMiddleware({ app })
+
   await new Promise(resolve => app.listen({ port: 4000 }, resolve))
+
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 }
 
