402a9ea7615740fa9b30a3b4845ccaed33cb6d94
Douglas Barone <douglasvbarone@gmail.com>
2021-10-03 18:33:49 -0400

    Alterado para apollo-server

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
-import { ApolloServer } from 'apollo-server-express'
-import express from 'express'
+import { ApolloServer } from 'apollo-server'
 
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
 
 import { pets, owners, categories } from './fakeDB'
 
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: () => ({ pets, owners, categories })
-  })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => ({ pets, owners, categories })
+})
 
-  await server.start()
-
-  const app = express()
-
-  server.applyMiddleware({ app })
-
-  await new Promise(resolve => app.listen({ port: 4000 }, resolve))
-
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-}
-
-startApolloServer(typeDefs, resolvers)
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
