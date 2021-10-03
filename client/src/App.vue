64fe5974c74929271b035ce6c51a2b54f194bc6d
Douglas Barone <douglasvbarone@gmail.com>
2021-10-03 19:41:25 -0400

    Adicionado cliente exemplo

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark><h1>GraphQL Pets</h1> </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+}
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col v-for="pet in pets" :key="pet.id" :cols="4">
+        <v-card>
+          <v-card-title>
+            {{ pet.name }} (<em>{{ pet.breed }}</em
+            >)
+          </v-card-title>
+          <v-card-text>
+            <small>{{ pet.category.name }}</small>
+            <div v-for="owner in pet.owners" :key="owner.id">
+              {{ owner.name }}
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { gql } from 'graphql-tag'
+export default {
+  name: 'Home',
+  apollo: {
+    pets: {
+      query: gql`
+        {
+          pets {
+            id
+            name
+            breed
+            owners {
+              id
+              name
+            }
+            category {
+              name
+            }
+          }
+        }
+      `
+    }
+  },
+  components: {}
+}
+</script>
